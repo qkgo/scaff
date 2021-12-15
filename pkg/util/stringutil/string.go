9588cdb422f22315a064b60c4cb976db5668c36f
monday <zhouyihaha@gmail.com>
2021-12-15 11:03:33 +0800

    Update string.go
    update method for duplication import

--- pkg/util/stringutil/string.go
+++ pkg/util/stringutil/string.go
@@ -6,6 +6,10 @@ func ContainsFold(a, b string) bool {
 	return strings.Contains(strings.ToLower(a), strings.ToLower(b))
 }
 
+func EqualFold(a, b string) bool {
+	return strings.EqualFold(a, b)
+}
+
 func CompareFold(a, b string) bool {
 	return strings.EqualFold(a, b)
 }
