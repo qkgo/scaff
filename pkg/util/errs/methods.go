6443c1f20e6cb310854178682135f70686e79523
monday <zhouyihaha@gmail.com>
2022-02-16 19:42:03 +0800

    upgrade log

--- pkg/util/errs/methods.go
+++ pkg/util/errs/methods.go
@@ -6,15 +6,15 @@ import (
 )
 
 func HandleError(err error) {
-	fmt.Printf("%v from %s", err, debug.Stack())
+	fmt.Printf("%v from %s", err, string(debug.Stack()))
 }
 
 func Handle(err error) {
-	fmt.Printf("%v from %s", err, debug.Stack())
+	fmt.Printf("%v from %s", err, string(debug.Stack()))
 }
 
 func PrintError(err error) {
 	if err != nil {
-		fmt.Printf("%v from %s", err, debug.Stack())
+		fmt.Printf("%v from %s", err, string(debug.Stack()))
 	}
 }
