4f21bb6d0c8fcf1076cc7f5647bec99e58d7b5dc
monday <zhouyihaha@gmail.com>
2022-02-28 14:47:48 +0800

    Update err-handle-util.go

--- pkg/util/err-handle-util.go
+++ pkg/util/err-handle-util.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/qkgo/scaff/pkg/cfg"
@@ -41,9 +42,15 @@ func ErrorCatchEncryption(err error, errorMessage string, c *gin.Context) bool {
 	return true
 }
 
-func MD5(str string) string {
+func md5Sum(str string) string {
 	w := md5.New()
 	io.WriteString(w, str)
 	md5str := fmt.Sprintf("%x", w.Sum(nil))
 	return md5str
+}
+
+func MD5(inputBytes []byte) string {
+	h := md5.New()
+	h.Write(inputBytes)
+	return hex.EncodeToString(h.Sum(nil))
 }
